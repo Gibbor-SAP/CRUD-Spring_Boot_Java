e3c158c12e27eb5e356b729d82a6167d1a769031
Gibbor28 <75150041+Gibbor-SAP@users.noreply.github.com>
2023-05-03 10:45:44 -0300

    Se movieron las carpetas direccion y medico a la paquete domain. Se creó el paquete infra con una clase para mapear los errores a nivel global y se trataron los errores 500 y 404 para crear un mejor lectura del error y evitar la exposicion de datos

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,6 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.DatosRegistroPaciente;
+import med.voll.api.domain.medico.DatosRegistroPaciente;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/med/voll/api/domain/medico/DatosRegistroPaciente.java
+++ src/main/java/med/voll/api/domain/medico/DatosRegistroPaciente.java
@@ -1,6 +1,6 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
-import med.voll.api.direccion.DatosDireccion;
+import med.voll.api.domain.direccion.DatosDireccion;
 
 public record DatosRegistroPaciente(String nombre, String email, String telefono, String documentoIdentidad, DatosDireccion direccion) {
 }
